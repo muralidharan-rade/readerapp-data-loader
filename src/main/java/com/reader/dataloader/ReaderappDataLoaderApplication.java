88152fa703d07f4065620729e60a501c0fe5424f
muralidharan-rade <muralidharan.balanandan@walmart.com>
2022-10-22 13:38:27 +0530

    basic data loading done

--- src/main/java/com/reader/dataloader/ReaderappDataLoaderApplication.java
+++ src/main/java/com/reader/dataloader/ReaderappDataLoaderApplication.java
@@ -1,27 +1,87 @@
 package com.reader.dataloader;
 
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Optional;
+import java.util.stream.Stream;
 
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.cassandra.CqlSessionBuilderCustomizer;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.reader.dataloader.author.Author;
+import com.reader.dataloader.author.AuthorRepository;
 import com.reader.dataloader.configuration.DataStaxAstraProperties;
 
 @SpringBootApplication
 @EnableConfigurationProperties(DataStaxAstraProperties.class)
 public class ReaderappDataLoaderApplication {
 
+	@Autowired
+	AuthorRepository authorRepository;
+
+	@Value("${dataloader.location.author}")
+	private String authorDumpPath;
+
+	@Value("${dataloader.location.work}")
+	private String workDumpPath;
+
 	public static void main(String[] args) {
 		SpringApplication.run(ReaderappDataLoaderApplication.class, args);
 	}
-	
+
 	@Bean
-    public CqlSessionBuilderCustomizer sessionBuilderCustomizer(DataStaxAstraProperties astraProperties) {
-        Path bundle = astraProperties.getSecureConnectBundle().toPath();
-        return builder -> builder.withCloudSecureConnectBundle(bundle);
-    }
+	public CqlSessionBuilderCustomizer sessionBuilderCustomizer(DataStaxAstraProperties astraProperties) {
+		Path bundle = astraProperties.getSecureConnectBundle().toPath();
+		return builder -> builder.withCloudSecureConnectBundle(bundle);
+	}
+
+	@PostConstruct
+	public void start() {
+		initAuthors();
+		initWorks();
+	}
+
+	private void initWorks() {
+		System.out.println("work location :: " + workDumpPath);
+
+	}
+
+	private void initAuthors() {
+		Path path = Paths.get(authorDumpPath);
+
+		try (Stream<String> lines = Files.lines(path)) {
+			lines.forEach(line -> {
+				try {
+					String authorString = line.substring(line.indexOf("{"));
+					JsonObject authorJsonObj = JsonParser.parseString(authorString).getAsJsonObject();
+
+					Author author = new Author();
+					author.setId(authorJsonObj.get("key").toString().replace("/authors/", ""));
+					author.setName(authorJsonObj.get("name").toString());
+					author.setPersonalName(Optional.ofNullable(authorJsonObj.get("personal_name"))
+							.map(JsonElement::toString).orElse(""));
+					authorRepository.save(author);
+				} catch (Exception ex) {
+					ex.printStackTrace();
+				}
+			});
+
+		} catch (IOException e) {
+			e.printStackTrace();
+
+		}
+	}
 
 }
---
+++ src/main/java/com/reader/dataloader/author/AuthorRepository.java
@@ -0,0 +1,9 @@
+package com.reader.dataloader.author;
+
+import org.springframework.data.cassandra.repository.CassandraRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AuthorRepository extends CassandraRepository<Author, String> {
+
+}
